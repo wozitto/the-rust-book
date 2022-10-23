8d806e3b687f8b6325df14c62415271341b73a15
wozitto <hirookatoshito@gmail.com>
2022-10-23 13:08:20 +0900

    Hello, Cargo!

---
+++ hello_cargo/target/debug/deps/hello_cargo-e2dae459e32315cc.d
@@ -0,0 +1,5 @@
+/Users/hirooka.toshito/private/the-rust-book/hello_cargo/target/debug/deps/hello_cargo-e2dae459e32315cc: src/main.rs
+
+/Users/hirooka.toshito/private/the-rust-book/hello_cargo/target/debug/deps/hello_cargo-e2dae459e32315cc.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/hirooka.toshito/private/the-rust-book/hello_cargo/target/debug/hello_cargo: /Users/hirooka.toshito/private/the-rust-book/hello_cargo/src/main.rs
